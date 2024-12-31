c5617ced6116297f37ee59fdbbc69b55307e753b
Tiger-Foxx <donfackarthur750@gmail.com>
2024-12-31 12:44:54 +0100

    global config

--- src/app/Home/home-header/home-header.component.ts
+++ src/app/Home/home-header/home-header.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-home-header',
   imports: [],
   templateUrl: './home-header.component.html',
+  standalone: true,
   styleUrl: './home-header.component.css'
 })
 export class HomeHeaderComponent {
